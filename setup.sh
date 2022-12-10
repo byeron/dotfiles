6d8fb1152da332ec7b8a59f52a6780adca650024
byeron <disce.uno@gmail.com>
2022-12-10 16:41:47 +0900

    a

--- setup.sh
+++ setup.sh
@@ -15,4 +15,4 @@ ln -sf /home/$USER/.profile /home/$USER/dotfiles/profile
 # brew
 echo "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-. /home/$USER/.progile
+. /home/$USER/.profile
