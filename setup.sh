1165b9315650fc675b4030d39cacf43422b06f49
byeron <disce.uno@gmail.com>
2022-12-10 16:56:36 +0900

    a

--- setup.sh
+++ setup.sh
@@ -12,6 +12,10 @@ echo $PASSWORD | sudo -kS apt install -y build-essential procps curl file git
 # brew
 echo "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+if [ -d /home/$USER/dotfiles ]; then
+	rm -rf /home/$USER/dotfiles
+fi
 git clone https://github.com/byeron/dotfiles.git /home/$USER/dotfiles
+
 ln -sf /home/$USER/dotfiles/profile /home/$USER/.profile
 . /home/$USER/.profile
