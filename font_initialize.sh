03172e8c3da9a66e9c3a134c96d229b13c6d9b04
byeron <disce.uno@gmail.com>
2022-12-11 17:04:03 +0900

    a

--- font_initialize.sh
+++ font_initialize.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -d /home/$USER/dotfiles ]; then
+        rm -rf /home/$USER/dotfiles
+fi
+git clone https://github.com/byeron/dotfiles.git /home/$USER/dotfiles
+
+source /home/$USER/.profile
+
+
 wget https://github.com/yuru7/HackGen/releases/download/v2.7.1/HackGen_NF_v2.7.1.zip
 unzip HackGen_NF_v2.7.1.zip
 mv HackGen_NF_v2.7.1 HackGen_NF
