87080c6b56586f35caf36c1d42ad278ef9b38bf5
byeron <disce.uno@gmail.com>
2022-12-11 11:02:22 +0900

    a

--- brew_initialize.sh
+++ brew_initialize.sh
@@ -4,6 +4,7 @@ if [ -d /home/$USER/dotfiles ]; then
         rm -rf /home/$USER/dotfiles
 fi
 git clone https://github.com/byeron/dotfiles.git /home/$USER/dotfiles
+
 source /home/$USER/.profile
 ln -sf /home/$USER/dotfiles/Brewfile /home/$USER/Brewfile
 
