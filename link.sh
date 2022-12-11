0e5a05f45b5ddf25a15f41ede5cf793885b5b4dc
byeron <disce.uno@gmail.com>
2022-12-11 21:20:43 +0900

    a

--- link.sh
+++ link.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -d /home/$USER/dotfiles ]; then
+        rm -rf /home/$USER/dotfiles
+fi
+git clone https://github.com/byeron/dotfiles.git /home/$USER/dotfiles
+
 ln -sf /home/$USER/dotfiles/bashrc /home/$USER/.bashrc
 ln -sf /home/$USER/dotfiles/profile /home/$USER/.profile
 ln -sf /home/$USER/dotfiles/vimrc /home/$USER/.vimrc
