e68e8b2d2ec9b00c19d7925d965a14e5eefc330f
byeron <disce.uno@gmail.com>
2022-12-11 14:29:23 +0900

    a

--- brew_initialize.sh
+++ brew_initialize.sh
@@ -9,4 +9,4 @@ source /home/$USER/.profile
 ln -sf /home/$USER/dotfiles/Brewfile /home/$USER/Brewfile
 
 brew bundle --file=/home/$USER/Brewfile
-$(brew --prefix)/opt/fzf/install
+yes y | $(brew --prefix)/opt/fzf/install
