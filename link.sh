b7365a1c40ae68ea841019032d95cb84334e4ee9
byeron <disce.uno@gmail.com>
2022-12-11 19:34:45 +0900

    a

--- link.sh
+++ link.sh
@@ -3,4 +3,4 @@
 ln -sf /home/$USER/dotfiles/bashrc /home/$USER/.bashrc
 ln -sf /home/$USER/dotfiles/profile /home/$USER/.profile
 ln -sf /home/$USER/dotfiles/vimrc /home/$USER/.vimrc
-ln -sfn /home/$USER/dotfiles/config /home/$USER/.config
+ln -sfn /home/$USER/dotfiles/config/ /home/$USER/.config/
