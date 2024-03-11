0e88f2c1f30c296ace3a67e05319bb1df7c18c44
byeron <35923134+byeron@users.noreply.github.com>
2024-03-11 18:36:29 +0900

    Update vim_initialize.sh

--- vim_initialize.sh
+++ vim_initialize.sh
@@ -5,6 +5,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir -p /home/$USER/.config/nvim/
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# mkdir -p /home/$USER/.config/nvim/
