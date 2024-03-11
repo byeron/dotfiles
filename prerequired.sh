0213d7e7ff8ece02ec264c1fe4fc137e8b8a7f15
byeron <35923134+byeron@users.noreply.github.com>
2024-03-11 13:44:39 +0900

    Update prerequired.sh

--- prerequired.sh
+++ prerequired.sh
@@ -11,7 +11,7 @@ echo $PASSWORD | sudo -kS apt install -y build-essential procps curl file git
 
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc)
+(echo; echo 'eval "$/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 
