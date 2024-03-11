4e90cbcee3e4bd67eb34cd15c3cdee4cf8108b86
byeron <35923134+byeron@users.noreply.github.com>
2024-03-11 14:08:34 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubuserconten
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/brew_initialize.sh)"
 # PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/font_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/vim_initialize.sh)"
-# PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/docker_initialize.sh)"
+PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/docker_initialize.sh)"
 # PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/cargo_bundle.sh)"
-# PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/link.sh)"
+PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/link.sh)"
