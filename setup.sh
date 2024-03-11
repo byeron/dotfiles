1fbfb97c1dd7e1d961503249ce9299910450f5e6
byeron <35923134+byeron@users.noreply.github.com>
2024-03-11 19:50:52 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,4 @@ PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubuserconten
 # PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/font_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/vim_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/docker_initialize.sh)"
-# PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/cargo_bundle.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/link.sh)"
