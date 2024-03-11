787075d944f4d1d4bbee20d1cab27d5545fd82e5
byeron <35923134+byeron@users.noreply.github.com>
2024-03-11 19:34:32 +0900

    Revert "Update docker_initialize.sh"
    This reverts commit 6f909ea4fc7a353c495f76f28e65a5284ec916f7.

--- docker_initialize.sh
+++ docker_initialize.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-echo $PASSWORD | for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get -y remove $pkg; done
+echo $PASSWORD | sudo apt -y remove docker docker-engine docker.io containerd runc
 echo $PASSWORD | sudo apt update
-echo $PASSWORD | sudo apt install -y ca-certificates curl
-echo $PASSWORD | sudo install -m 0755 -d /etc/apt/keyrings
-echo $PASSWORD | sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-echo $PASSWORD | sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo $PASSWORD | sudo apt install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+echo $PASSWORD | sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo $PASSWORD | sudo apt-get update
-echo $PASSWORD | sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
+echo $PASSWORD | sudo apt update
+echo $PASSWORD | sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
