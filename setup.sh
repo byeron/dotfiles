5ac54d3d4db800517eeb26570624392ce8cf2778
byeron <35923134+byeron@users.noreply.github.com>
2024-03-12 11:03:15 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubuserconten
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/vim_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/docker_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/xrdp_initialize.sh)"
-PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/enhanced-session-mode.sh)"
+# PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/enhanced-session-mode.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/link.sh)"
