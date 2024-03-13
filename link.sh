2903c2b076e57e540ce54194a17318e715a2c673
byeron <35923134+byeron@users.noreply.github.com>
2024-03-13 11:31:20 +0900

    Update link.sh

--- link.sh
+++ link.sh
@@ -10,5 +10,6 @@ ln -sf /home/$USER/dotfiles/profile /home/$USER/.profile
 ln -sf /home/$USER/dotfiles/vimrc /home/$USER/.vimrc
 ln -sf /home/$USER/dotfiles/gitconfig /home/$USER/.gitconfig
 ln -sf /home/$USER/dotfiles/gitmessage.txt /home/$USER/.gitmessage.txt
+ln -sf /home/$USER/dotfiles/xsessionrc /home/$USER/.xsessionrc
 
 ln -sfn /home/$USER/dotfiles/config/ /home/$USER/.config
