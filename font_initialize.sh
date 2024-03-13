ba0739b23cc6ee1d9abe90ce2827768e1b8514fb
byeron <35923134+byeron@users.noreply.github.com>
2024-03-13 17:47:50 +0900

    Update font_initialize.sh

--- font_initialize.sh
+++ font_initialize.sh
@@ -15,9 +15,10 @@ echo $PASSWORD | sudo mv HackGen_NF /usr/share/fonts/
 fc-cache -fv
 rm HackGen_NF_v2.9.0.zip
 
+# !反映されなかったので、手作業で行う
 # ターミナルのデフォルトの設定の ID を取得
-default_profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
-default_profile=${default_profile:1:-1}  # シングルクォートを削除
+# default_profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
+# default_profile=${default_profile:1:-1}  # シングルクォートを削除
 
 # デフォルトの設定の ID に対してフォントを設定する
-gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$default_profile/" font 'HackGen Console NF 14'
+# gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$default_profile/" font 'HackGen Console NF 14'
