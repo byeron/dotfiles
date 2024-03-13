1d3103f13efb53426cd99a7f2fa8748cf8819a22
byeron <35923134+byeron@users.noreply.github.com>
2024-03-13 16:45:03 +0900

    Update font_initialize.sh

--- font_initialize.sh
+++ font_initialize.sh
@@ -8,9 +8,9 @@ git clone https://github.com/byeron/dotfiles.git /home/$USER/dotfiles
 source /home/$USER/.profile
 
 
-wget https://github.com/yuru7/HackGen/releases/download/v2.7.1/HackGen_NF_v2.7.1.zip
-unzip HackGen_NF_v2.7.1.zip
-mv HackGen_NF_v2.7.1 HackGen_NF
+wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip
+unzip HackGen_NF_v2.9.0.zip
+mv HackGen_NF_v2.9.0 HackGen_NF
 echo $PASSWORD | sudo mv HackGen_NF /usr/share/fonts/
 fc-cache -fv
-rm HackGen_NF_v2.7.1.zip
+rm HackGen_NF_v2.9.0.zip
