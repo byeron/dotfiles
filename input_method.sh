ba4cd7e665b47563c9046b92042679932474cd10
byeron <35923134+byeron@users.noreply.github.com>
2024-03-13 15:48:50 +0900

    Update input_method.sh

--- input_method.sh
+++ input_method.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo $PASSWORD | sudo apt -y install fcitx5-mozc fcitx-bin
+echo $PASSWORD | sudo apt -y install fcitx5-mozc
 echo $PASSWORD | sudo apt -y remove ibus-mozc ibus-table ibus-gtk ibus-gtk3
-echo $PASSWORD | sudo apt -y install fcitx-bin
+echo $PASSWORD | sudo apt -y install fcitx-bin fcitx
 im-config -n fcitx5
 sed -i "/EnabledIMList/s/mozc:False/mozc:True/g" ~/.config/fcitx/profile
