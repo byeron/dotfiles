bd7a30f962811d327da61e52b21567953f96b9e7
byeron <35923134+byeron@users.noreply.github.com>
2024-03-13 18:04:17 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/prerequired.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/brew_initialize.sh)"
-# PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/font_initialize.sh)"
+PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/font_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/vim_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/docker_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/xrdp_initialize.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/input_method.sh)"
-# PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/enhanced-session-mode.sh)"
 PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/link.sh)"
+PASSWORD=$PASSWORD USER=$USER bash -c "$(curl -fsSL https://raw.githubusercontent.com/byeron/dotfiles/main/terminal_initialize.sh)"
